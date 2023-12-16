5d0dca992e8e16c8965113ab10b1ffbf8e474a35
Thomas Musial <th896493@dal.ca>
2023-12-16 17:08:11 -0400

    responisve herobanner shenanagins

---
+++ .history/src/components/outro_20231216160125.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import '../styles/outro.css'
+
+const Outro = () => {
+
+
+    return (
+        <div className="outro-container">
+            <h2 className="title-text outro-title ">
+                Elevate Your Digital Presence
+            </h2>
+            <p className="description-text">
+            Elevate your brand online with Q3 Visuals. Our creative and scientific approach ensure a website that stands out and gives you the <span className='bold-700'>best possible</span>
+            chance to elevate your awesome brand.
+            </p>
+            <button className="button">Transform your brand</button>
+        </div>
+    )
+}
+
+export default Outro
