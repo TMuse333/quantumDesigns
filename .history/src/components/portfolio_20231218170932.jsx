c618112eeee938e69fe704ade80bdb69385f7574
Thomas Musial <th896493@dal.ca>
2023-12-18 19:23:28 -0400

    started responsive design

---
+++ .history/src/components/portfolio_20231218170932.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import Navbar from "./navbar";
+import Footer from "./footer";
+import '../styles/portfolio.css'
+import resume from '../images/resume.png'
+import card from '../images/quantum-card-game.png'
+
+
+const Portfolio = () => {
+
+
+    return (
+        <div className="portfolio-container">
+            <h1 className="title-text">
+                Our Work
+            </h1>
+            <p className="description-text">
+                We are dedicated to
+                creating the best content that
+                we can, here is some of our work.
+                Your masterpiece could be here next!
+            </p>
+
+                <div className="personal-websites">
+
+                
+            <h2 className="title-text">
+               Personal Websites
+            </h2>
+            <p className="description-text">
+                Is your google docs resume not getting you
+                any callbacks? We can put you one step ahead of
+                your competition with an individualized website
+                showcasing how great of a person you are and why
+                an employer would be extremely lucky to have you.
+            </p>
+
+            <img src={resume}
+            style={{
+                width:'90vw',
+                maxWidth:'500px'
+            }}/>
+
+            <a href="https://thomasmusial.com">view</a>
+
+            </div>
+
+            <div className="company-websitess">
+                <h2 className="title-text">
+                    Company Websites
+                </h2>
+                <p className="description-text">
+                    Do you want to generate more business 
+                    for your company? Q3 Visuals has got you covered.
+                    We will put your product and services on a visually appealing
+                    display, showcasing its greatness and taking your business to the next level
+                </p>
+            </div>
+
+            <div className="other-projects">
+                <h2 className="title-text">
+                    Other Projects
+                </h2>
+                <p className="description-text">
+                    We are always looking to create innovative projects
+                    and adapt to whatever has to be done, here are some other projects
+                    of ours that you can check out.
+                </p>
+
+                    <h2 className="title-text">
+                        The Quantum Card Game
+                    </h2>
+
+                    <img src={card}
+                    style={{
+                        width:'300px'
+                    }}
+                    />
+
+                    <p className="description-text">
+                        Looking to test your memory and quick thinking skills?
+                        The quantum card game is a <span className="bold-700">
+                            fantastic </span> way to do so while viewing some
+                            fantastic cards. Can you score over 1000 points?
+                            
+                    </p>
+
+                    <a href=
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Portfolio
---
+++ .history/src/components/tenets_20231218183656.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+
+import davinci from '../images/davinci-quote.jpg'
+import { Link } from 'react-router-dom';
+
+import '../styles/tenets.css'
+
+
+const Tenets = () => {
+  
+
+
+  return (
+  
+
+   
+    
+      <div className="tenets-container">
+      <h2 className="title-text committed"><span className="bold-700">Committed</span> To <span className="bold-700">Creativity,</span> </h2>
+      <h2 className='title-text committed inspired'><span className="bold-700">Inspired </span>By <span className="bold-700">Science. </span></h2>
+
+<div className='tenets-description'>
+  
+</div>
+      <img src={davinci}
+      style={{
+        width:'100vw'
+      }}/>
+<p className='description-text'>
+    With so much content on the web today, standing out is crucial. We have formulated a unique process
+    that uses the power of psychology, engaging visual tactics
+    and creativity to ensure<br/> <span className="bold-700">your website stands out above the rest</span>.
+
+</p>
+<Link to='/process'
+>
+<button className='button'>
+  Learn our tenets of work
+</button>
+</Link>
+
+       
+      </div>
+
+  );
+};
+
+export default Tenets;
