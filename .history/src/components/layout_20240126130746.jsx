f191af0f4d03da3228a638b779f6e05470e26b21
Thomas Musial <th896493@dal.ca>
2024-01-26 13:31:07 -0400

    time to sauce up the infographic

---
+++ .history/src/components/layout_20240126130746.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect,useRef } from "react";
+import '../app.css';
+import WorkCarousel from "./work";
+import Testimonials from "./testimonials";
+import Outro from "./outro";
+import Footer from "./footer";
+import Navbar from "./navbar";
+
+import { content1 } from "../componentData/data";
+import { content2 } from "../componentData/data";
+import Content from "./content";
+import Herobanner2 from "./herobanner2";
+import ServiceOffering from "./serviceOffering";
+import Hero3 from "./herobanner3";
+import { useState } from "react";
+import ParticlesComponent from "./particles";
+
+const Layout = () => {
+
+    const [content1Animated, setContent1Animated] = useState(false)
+    const [content2Animated, setContent2Animated] = useState(false)
+    
+
+const layoutRef = useRef()
+
+
+
+const [scrollY, setScrollY] = useState(0);
+
+
+
+    return (
+   
+        <>
+
+<ParticlesComponent/>
+ 
+        <div className="layout" id="layout"
+        >
+            <Navbar />
+            <Hero3 />
+            <Content  />
+            <ServiceOffering />
+            <Content 
+          
+             />
+            {/* <WorkCarousel /> */}
+            {/* <Testimonials /> */}
+            <Outro title='transform your vision today' content={content1.description} />
+            <Footer />
+        </div>
+        
+</>
+       
+    );
+}
+
+export default Layout;
---
+++ .history/src/components/layout_20240126131601.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect,useRef } from "react";
+import '../app.css';
+import WorkCarousel from "./work";
+import Testimonials from "./testimonials";
+import Outro from "./outro";
+import Footer from "./footer";
+import Navbar from "./navbar";
+
+import { content1 } from "../componentData/data";
+import { content2 } from "../componentData/data";
+import Content from "./content";
+import Herobanner2 from "./herobanner2";
+import ServiceOffering from "./serviceOffering";
+import Hero3 from "./herobanner3";
+import { useState } from "react";
+import ParticlesComponent from "./particles";
+
+const Layout = () => {
+
+    const [content1Animated, setContent1Animated] = useState(false)
+    const [content2Animated, setContent2Animated] = useState(false)
+    
+
+const layoutRef = useRef()
+
+
+
+const [scrollY, setScrollY] = useState(0);
+
+
+
+    return (
+   
+        <>
+
+<ParticlesComponent/>
+ 
+        <div className="layout" id="layout"
+        >
+            <Navbar />
+            <Hero3 
+            />
+            <Content    id='about1'
+            hasTilt={true}
+            hasAnimation={true}
+             />
+            <ServiceOffering />
+            <Content 
+          id='about2'
+          hasTilt={true}
+          hasAnimation={true}
+             />
+            {/* <WorkCarousel /> */}
+            {/* <Testimonials /> */}
+            <Outro title='transform your vision today' content={content1.description} />
+            <Footer />
+        </div>
+        
+</>
+       
+    );
+}
+
+export default Layout;
