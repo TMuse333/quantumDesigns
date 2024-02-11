ac38bb15b20cc427c48ab0a3ce07855d32897331
Thomas Musial <th896493@dal.ca>
2024-02-11 10:08:20 -0400

    website ready to deploy, just need to optimize images

---
+++ .history/src/components/footer/footer_20240211081930.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+      
+        
+       
+      <div className="quick-links">
+        {links.map((link,index) => (
+          <Link to={link.dest}
+          className='link'
+          key={index}>
+            {link.name}
+          </Link>
+        ))}
+        </div>
+   
+       
+      </div>
+
+      <p>Q3 Creations 2024</p>
+    
+    </footer>
+  );
+};
+
+export default Footer;
